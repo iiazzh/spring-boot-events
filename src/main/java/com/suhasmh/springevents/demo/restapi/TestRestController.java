c3091be7c94fea538161ccd9c3beec1335459dfa
suhas <86886386+iiazzh@users.noreply.github.com>
2021-11-12 22:27:24 +0530

    Update TestRestController.java

--- src/main/java/com/suhasmh/springevents/demo/restapi/TestRestController.java
+++ src/main/java/com/suhasmh/springevents/demo/restapi/TestRestController.java
@@ -1,23 +1,15 @@
 package com.suhasmh.springevents.demo.restapi;
 
-import com.suhasmh.springevents.demo.daemon.publisher.MyPublisher;
 import com.suhasmh.springevents.demo.model.request.Application;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestRestController {
-    private final MyPublisher myPublisher;
-
-    public TestRestController(MyPublisher myPublisher) {
-        this.myPublisher = myPublisher;
-    }
-
     @GetMapping("/test")
-    public Application getAPerson() {
+    public Application testApplication() {
         var app = new Application();
         app.setStatus("Running");
-        myPublisher.publishCustomEvent("Suhas publishes an event message");
         return app;
     }
 }
