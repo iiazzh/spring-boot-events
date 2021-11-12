8192fb2fbd8a5a43c9fc5385066bdb144e886128
suhas <86886386+iiazzh@users.noreply.github.com>
2021-11-12 21:52:51 +0530

    Update MyPublisher.java

--- src/main/java/com/suhasmh/springevents/demo/daemon/publisher/MyPublisher.java
+++ src/main/java/com/suhasmh/springevents/demo/daemon/publisher/MyPublisher.java
@@ -1,14 +1,17 @@
 package com.suhasmh.springevents.demo.daemon.publisher;
 
 import com.suhasmh.springevents.demo.model.MessageToPublish;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyPublisher {
+public class MyPublisher extends ApplicationEvent {
     private final ApplicationEventPublisher applicationEventPublisher;
 
-    public MyPublisher(ApplicationEventPublisher applicationEventPublisher) {
+    public MyPublisher(ApplicationEventPublisher applicationEventPublisher, Object source) {
+        //Call to 'super()' must be first statement in constructor body
+        super(source);
         this.applicationEventPublisher = applicationEventPublisher;
     }
 
